0b43ca2f0daaaeed4db323f204edf78dd61bbd88
Johab <freitasjohab@gmail.com>
2022-08-18 01:05:08 -0300

    Page

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -1,11 +1,12 @@
 package com.api.parkingcontrol.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.api.parkingcontrol.models.ParkingSpotModel;
@@ -42,9 +43,9 @@ public class ParkingSpotService {
 		return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
 	}
 
-	public List<ParkingSpotModel> findAll() {
+	public Page<ParkingSpotModel> findAll(Pageable pageable) {
 
-		return parkingSpotRepository.findAll();
+		return parkingSpotRepository.findAll(pageable);
 	}
 
 	public Optional<ParkingSpotModel> findById(UUID id) {
